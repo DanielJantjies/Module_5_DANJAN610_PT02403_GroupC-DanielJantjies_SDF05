61f4b5e2dc061d7fd191a62cded4ed08f9cdf2a5
DanielJantjies <162917612+DanielJantjies@users.noreply.github.com>
2024-04-18 22:46:21 +0200

    Edits up until "Add new button"

--- index.js
+++ index.js
@@ -3,9 +3,42 @@
 
 // 2. Create a variable, sum, and set it to the sum of the two cards
 
-let firstCard = 6
-let secondCard = 9
-
+let firstCard = 10
+let secondCard = 4
 let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let message = ""
+let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
-console.log(sum)
+function startGame(){
+
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    sumEl.textContent = "Sum: " + sum
+if (sum < 21 ){
+    message = "Do you want to draw a new card? "
+} else if ( sum === 21){
+    message = "You've got BlackJack! "
+    hasBlackJack = true
+} else if ( sum > 21){
+    message = "You're out of the game! "
+    isAlive = false
+}
+
+messageEl.textContent = message
+
+}
+
+function newCard() {
+    console.log("Drawing a new card from the deck!")
+}
+
+
+
+
+
+
+
+
